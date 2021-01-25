653e55c0f7f685645227b0adc33f05caafb49eb5
tanmayjagtap <tanmayjagtap27@gmail.com>
2021-01-25 19:55:07 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Counter from './Components/Counter/Counter';
+import Footer from './Components/Footer/Footer';
+import ContactUs from './Components/ContactUs/ContactUs';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Counter/>
+      <ContactUs/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/ContactUs/ContactUs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ContactUs = () => {
+    return(
+        <section>
+            <span>
+                <div>
+                    <img src='https://icon2.cleanpng.com/20180209/yfq/kisspng-dumbbell-weight-training-olympic-weightlifting-bar-cartoon-dumbbell-5a7d55d7b2a2e2.9743379815181634157317.jpg' alt='bumbell'/>
+                </div>
+            </span>
+            <span>
+                
+            </span>
+        </section>
+    );
+}
+
+export default ContactUs;
