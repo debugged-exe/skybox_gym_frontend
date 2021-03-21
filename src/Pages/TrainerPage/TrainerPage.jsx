0d5c1a99668c6dc0bdcf9387ce44c41dea345929
imrehan <rehanshaikh2156@gmail.com>
2021-03-22 03:20:31 +0530

    fixed errors

--- src/Pages/TrainerPage/TrainerPage.jsx
+++ src/Pages/TrainerPage/TrainerPage.jsx
@@ -24,21 +24,22 @@ const TrainerPage = () => {
 
 	 }).then(res=>res.json())
 	 .then(res=>{
-		 setCurrUserDet(res)
+		 setCurrUserDet(res.email)
 	 })
-		
-		fetch('https://skybox-athlete.herokuapp.com/get-my-clients/6036b7774cb92d00152ba9d7', {
+
+   const detailID = localStorage.getItem('det_id')
+		fetch('https://skybox-athlete.herokuapp.com/get-my-clients/'+detailID, {
 
 	 }).then(res=>res.json())
 	 .then(res=>{
 		 setTrainees(res)
 	 })
-  },[])
+  })
 
         return(
             <div className='flex justify-center items-center flex-column'>
                 <div>
-                    <h1>{currUserDet.email}</h1>
+                    <h1>{currUserDet}</h1>
                     <h3>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</h3>
                 </div>
                 <p className="f6 link dim ph3 pv2 mb2 dib white bg-dark-blue pointer" onClick={()=>setInfoModal(true)}>Update Info</p>
